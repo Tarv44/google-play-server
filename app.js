b30ff57a0b3be30bd4965bc3f08d5c87f472f73f
Jonah Tarver <jonahdevine@gmail.com>
2020-12-31 00:53:41 -0800

    Assignment complete

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const apps = require('./app-data');
+
+const app = express();
+
+app.get('/apps', (req, res) => {
+    const {sort, genres = ''} = req.query;
+
+    if(sort) {
+        if(!['Rating', 'App'].includes(sort)) {
+            return res
+                .status(400)
+                .send('Sort must be rating or app.')
+        }
+    }
+    
+
+    const genreOptions = ['Action', 'Puzzle', 'Strategy', 'Casual', 'Arcade', 'Card']
+
+    if(genres.length > 0 && !genreOptions.includes(genres)) {
+        return res
+            .status(400)
+            .send('Genres must be Action, puzzle, strategy, casual, arcade, or card.')
+    }
+
+    let results = apps
+        .filter(app => 
+            app
+                .Genres
+                .toLowerCase()
+                .includes(genres.toLowerCase()));
+    
+    if (sort) {
+        results.sort((a, b) => {
+            return a[sort] > b[sort] ? 1 : a[sort] < b[sort] ? -1 : 0
+        })
+    }
+
+    res.send(results)
+})
+
+app.listen(8000, () => {
+    console.log('Server started on PORT 8000');
+  });
